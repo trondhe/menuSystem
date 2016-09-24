3e9375bd98bf5c5c67ef307a680965fe55fdf22f
Trond H Emaus <trondhe@gmail.com>
2016-09-24 13:33:43 +0200

    Initial

---
+++ menuSystem/menuSystem.cpp
@@ -0,0 +1,94 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include "stdafx.h"
+
+
+
+void printscreen(char frame[8][21]) {
+	for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < 21; i++) {
+			printf("%c", frame[j][i]);
+		}
+		printf("\r\n");
+	}
+
+}
+
+
+int main()
+{
+	char buffer[8][21];
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 21; j++) {
+			buffer[i][j] = 219;
+		}
+	}
+
+	int menuctrl_state = 0;
+	int menuctrl_depth = 0;
+
+	int menuctrl = 0;
+
+
+
+	while (1) {
+
+
+		switch (menuctrl) {
+		case 119:	//up
+			if (menuctrl_state == 0) {
+				menuctrl_state = 7;
+				break;
+			}
+			else {
+				menuctrl_state -= 1;
+			}
+			break;
+
+		case 115:	//down
+			if (menuctrl_state == 7) {
+				menuctrl_state = 0;
+				break;
+			}
+			else {
+				menuctrl_state += 1;
+			}
+			break;
+
+		case 100:	//right
+			//entermenu();
+			break;
+
+		case 97:	//left
+			//backmenu();
+			break;
+		}
+
+		for (int i = 0; i < 8; i++) {
+			if (i != menuctrl_state)
+			{
+				buffer[i][0] = 255;
+			}
+			else {
+				buffer[i][0] = 175;
+			}
+		}
+
+
+		system("cls");
+		printscreen(buffer);
+		printf("%i\n", menuctrl);
+		printf("%i", menuctrl_state);
+		menuctrl = _getch();
+
+
+		//itoa()
+
+
+
+		//_sleep(100);
+
+	}
+	return 0;
+}
