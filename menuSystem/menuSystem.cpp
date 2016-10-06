a643320015bec23d5c82a008c072534fc8cadc1a
Trond H Emaus <trondhe@gmail.com>
2016-10-06 16:56:19 +0200

    Display of main struct working

--- menuSystem/menuSystem.cpp
+++ menuSystem/menuSystem.cpp
@@ -1,18 +1,9 @@
 ﻿#include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
 
-typedef struct node_type {
-	int val;
-	char** screendata;
-	
-	struct node_type *node_nxt;
-	struct node_type *node_pre;
-
-} node_type;
-
-node_type node_main;
-//node_main->screendata = { "kuk", "i", "kurry" };
-//node_main->val = 5;
 
 void printscreen(char frame[8][21]) {
 	for (int j = 0; j < 8; j++) {
@@ -27,7 +18,49 @@ void printscreen(char frame[8][21]) {
 
 int main()
 {
-	printf("%d", node_main.val);
+
+	typedef struct node_t {
+		char** scrndata;
+		struct node_t *node_nxt;
+		struct node_t *node_pre;
+	} node_t;
+
+	char* scrndata_hved[8] = { "Play", "Highscore", "Settings", "Easteregg", "test", "6", "7", "8" };
+	char* scrndata_play[8] = { "Normal", "Hard", "u w0t m8", "4", "5", "6", "7", "8" };
+	char* scrndata_sett[8] = { "Tuning", "", "Settings", "Easteregg", "test", "6", "7", "8" };
+	char* scrndata_east[8] = { "Play", "Highscore", "Settings", "Easteregg", "test", "6", "7", "8" };
+
+	node_t node_hved;
+	node_t node_play;
+	node_t node_sett;
+	node_t node_east;
+
+	node_hved.node_pre = NULL;
+	node_hved.node_nxt = &node_play;
+	//node_hved.node_nxt[1] = &node_sett;
+	//node_hved.node_nxt[2] = &node_east;
+
+	node_play.node_pre = &node_hved;
+	node_sett.node_pre = &node_hved;
+	node_east.node_pre = &node_hved;
+
+	node_play.node_nxt = NULL;
+	node_sett.node_nxt = NULL;
+	node_east.node_nxt = NULL;
+
+	node_hved.scrndata = (char**)malloc(sizeof(char*) * 8);
+	node_play.scrndata = (char**)malloc(sizeof(char*) * 8);
+	node_sett.scrndata = (char**)malloc(sizeof(char*) * 8);
+	node_east.scrndata = (char**)malloc(sizeof(char*) * 8);
+
+	for (int i = 0; i < 8; i++) {
+		node_hved.scrndata[i] = scrndata_hved[i];
+		node_play.scrndata[i] = scrndata_play[i];
+		node_sett.scrndata[i] = scrndata_sett[i];
+		node_east.scrndata[i] = scrndata_east[i];
+	}
+
+
 	char buffer[8][21];
 
 	for (int i = 0; i < 8; i++) {
@@ -37,15 +70,13 @@ int main()
 	}
 
 	int menuctrl_state = 0;
-	int menuctrl_depth = 0;
 	int menuctrl = 0;
 
+	node_t* node_current = &node_hved;
 
 
 
 	while (1) {
-
-
 		switch (menuctrl) {
 		case 119:	//up
 			if (menuctrl_state == 0) {
@@ -86,6 +117,11 @@ int main()
 			}
 		}
 
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < strlen((*node_current).scrndata[i]); j++) {
+				buffer[i][j+1] = (*node_current).scrndata[i][j];
+			}
+		}
 
 		system("cls");
 		printscreen(buffer);
