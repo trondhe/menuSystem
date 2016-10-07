e229a223f08ca0722c40d6815f3006c57b773feb
Trond H Emaus <trondhe@gmail.com>
2016-10-07 13:01:24 +0200

    node_init "done". Print missing

--- menuSystem/menuSystem.cpp
+++ menuSystem/menuSystem.cpp
@@ -8,19 +8,26 @@ typedef struct node_t {
 	char node_name[21];
 	struct node_t *node_prnt;
 	struct node_t **node_chld;
+	int node_chld_count;
 } node_t;
 
-node_t* node_init(int num_pointers, char* name)
+node_t* node_init(int num_pointers, char* name, node_t* parent)
 {
 	node_t* node = (node_t*)malloc(sizeof(node_t));
 	if (node == NULL){
 		return NULL;
 	}
+	node->node_chld_count = 0;
 	strcpy(node->node_name, name);
 	node->node_prnt = (node_t**)malloc(sizeof(node_t*));
+	node->node_prnt = parent;
 	node->node_chld = (node_t**)malloc(sizeof(node_t*) * num_pointers);
 	for (int i = 0; i < 8; i++){
 		node->node_chld[i] = NULL;
+	}
+	if ((parent->node_chld[parent->node_chld_count] == NULL) && (parent->node_chld_count < num_pointers)) {
+		parent->node_chld[parent->node_chld_count] = node;
+		parent->node_chld_count++;
 	}
 	return node;
 }
@@ -36,12 +43,32 @@ void printscreen(char frame[8][21]) {
 
 int main()
 {
-	//node_t* node_ = node_init(8, "");
-	node_t* node_mthr = node_init(8, "Home");
-	node_t* node_play = node_init(8, "Play");
-	node_t* node_sett = node_init(8, "Settings");
-	node_t* node_high = node_init(8, "Highscore");
-	node_t* node_estr = node_init(8, "Easteregg");
+	//NODE TEMPLATE
+	// 	node_t* node_ = node_init(8, "", &node_);
+	
+	// MOTHER NODE
+	node_t* node_mthr = node_init(8, "Home", NULL);
+
+	// CHILD NODES,	CHILD OF CHILD, ...
+	node_t* node_play = node_init(8, "Play", &node_mthr);
+		node_t* node_easy = node_init(8, "Easy", &node_play);
+		node_t* node_medi = node_init(8, "Medium", &node_play);
+		node_t* node_hard = node_init(8, "Hard", &node_play);
+		node_t* node_uwmt = node_init(8, "U wut m8", &node_play);
+
+	node_t* node_sett = node_init(8, "Settings", node_mthr);
+		node_t* node_tuni = node_init(8, "Tuning", &node_sett);
+		node_t* node_dead = node_init(8, "Deadzone", &node_sett);
+
+	node_t* node_high = node_init(8, "Highscore", node_mthr);
+		node_t* node_trond = node_init(8, "Trond", &node_high);
+		node_t* node_lars = node_init(8, "Lars", &node_high);
+
+	node_t* node_estr = node_init(8, "Easteregg", node_mthr);
+		node_t* node_kuk = node_init(8, "Kuk", &node_estr);
+		node_t* node_i = node_init(8, "i", &node_estr);
+		node_t* node_kurry = node_init(8, "Curry", &node_estr);
+		node_t* node_dickbutt = node_init(8, "Dickbutt", &node_estr);
 
 
 	/*typedef struct node_t {
@@ -128,17 +155,17 @@ int main()
 			break;
 
 		case 100:	//right
-			if ((*node_current).node_nxt[menuctrl_state] == NULL) {
+			if ((*node_current).node_chld[menuctrl_state] == NULL) {
 				break;
 			}
-			node_current = (*node_current).node_nxt[menuctrl_state];
+			node_current = (*node_current).node_chld[menuctrl_state];
 			break;
 
 		case 97:	//left
-			if ((*node_current).node_pre == NULL) {
+			if ((*node_current).node_prnt == NULL) {
 				break;
 			}
-			node_current = (*node_current).node_pre;
+			node_current = (*node_current).node_prnt;
 			break;
 		}
 
